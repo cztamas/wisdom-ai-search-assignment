1c721b1330e70aeb921f680a9e8c0147aaac5c59
cztamas <tamas.czank@gmail.com>
2023-12-07 11:41:00 +0100

    dummy search UI

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App" sx={{ p: 4 }}>
+      <Typography variant="h2">Search</Typography>
+      <Box className="search">
+        <TextField
+          fullWidth
+          sx={{ maxWidth: '600px', pb: 2 }}
+          label="Search in movies and full random files"
+          variant="outlined"
+        />
+        <Button variant="contained">Search</Button>
+      </Box>
+    </Box>
   );
 }
 
